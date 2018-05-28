fdfe49b9cf4e4b8726dca740afdc1178669f5160
marcmatias <marcmatias@me.com>
2018-05-28 11:00:09 -0300

    corrigindo untracked files

--- sistema/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Estabelecimento, Predio, Sala, Consumo
-
-# Register your models here.
-
-admin.site.register(Estabelecimento)
-admin.site.register(Predio)
-admin.site.register(Sala)
-admin.site.register(Consumo)
